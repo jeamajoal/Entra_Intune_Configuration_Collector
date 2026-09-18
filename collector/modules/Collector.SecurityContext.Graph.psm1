2af2931ea70de20614a692ec323a61a444e61d5f
Jeamajoal <jeamajoal@gmail.com>
2026-09-17 19:35:40 -0700

    fix: retain terminal Graph authentication state

--- collector/modules/Collector.SecurityContext.Graph.psm1
+++ collector/modules/Collector.SecurityContext.Graph.psm1
@@ -17,6 +17,8 @@ function New-CollectorGraphAuthState {
     $state = [pscustomobject]@{
         currentToken = if ([string]::IsNullOrWhiteSpace($GraphToken)) { $null } else { $GraphToken }
         tokenProvider = $GraphTokenProvider
+        terminalFailure = $false
+        terminalMessage = $null
     }
     $state.PSObject.TypeNames.Insert(0, $script:GraphAuthStateTypeName)
     return $state
@@ -32,6 +34,43 @@ function Test-CollectorGraphAuthState {
     return (
         $null -ne $AuthInput -and
         $AuthInput.PSObject.TypeNames -contains $script:GraphAuthStateTypeName
+    )
+}
+
+function Set-CollectorGraphAuthenticationTerminalState {
+    [CmdletBinding()]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '', Justification = 'This function only updates the invocation-local in-memory Graph authentication state.')]
+    param(
+        [AllowNull()]
+        [object]$AuthInput,
+
+        [Parameter(Mandatory = $true)]
+        [string]$Message
+    )
+
+    if (-not (Test-CollectorGraphAuthState -AuthInput $AuthInput)) {
+        return
+    }
+
+    $AuthInput.currentToken = $null
+    $AuthInput.terminalFailure = $true
+    $AuthInput.terminalMessage = $Message
+}
+
+function Test-CollectorGraphAuthenticationTerminalState {
+    [CmdletBinding()]
+    param(
+        [AllowNull()]
+        [object]$AuthInput
+    )
+
+    if (-not (Test-CollectorGraphAuthState -AuthInput $AuthInput)) {
+        return $false
+    }
+
+    return (
+        $AuthInput.PSObject.Properties.Match('terminalFailure').Count -gt 0 -and
+        [bool]$AuthInput.terminalFailure
     )
 }
 
@@ -103,6 +142,19 @@ function Get-CollectorGraphAccessToken {
         throw 'Unsupported Microsoft Graph authentication input. Expected a token string or collector Graph auth state.'
     }
 
+    if (Test-CollectorGraphAuthenticationTerminalState -AuthInput $AuthInput) {
+        $terminalMessage = if (
+            $AuthInput.PSObject.Properties.Match('terminalMessage').Count -gt 0 -and
+            -not [string]::IsNullOrWhiteSpace([string]$AuthInput.terminalMessage)
+        ) {
+            [string]$AuthInput.terminalMessage
+        }
+        else {
+            'Microsoft Graph authentication is terminal for this collector invocation.'
+        }
+        throw $terminalMessage
+    }
+
     $tokenProvider = if (
         $AuthInput.PSObject.Properties.Match('tokenProvider').Count -gt 0 -and
         $AuthInput.tokenProvider -is [scriptblock]
@@ -142,5 +194,7 @@ function Get-CollectorGraphAccessToken {
 Export-ModuleMember -Function @(
     'New-CollectorGraphAuthState',
     'Get-CollectorGraphAccessToken',
+    'Set-CollectorGraphAuthenticationTerminalState',
+    'Test-CollectorGraphAuthenticationTerminalState',
     'Test-CollectorGraphRefreshAvailable'
 )
